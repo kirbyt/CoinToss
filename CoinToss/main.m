fbee60d0ce236f0f99cdf12f0de307334093f7da
Kirby Turner <kirby@thecave.com>
2011-03-03 11:52:01 -0500

    Initial import.

---
+++ CoinToss/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CoinToss
+//
+//  Created by Kirby Turner on 3/3/11.
+//  Copyright 2011 White Peak Software Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+   NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+   // insert code here...
+   NSLog(@"Hello, World!");
+
+   [pool drain];
+    return 0;
+}
+
